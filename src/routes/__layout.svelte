f374886124d21214f952477a62ebc93ebe97adfb
0xvv <public_double_v@protonmail.com>
2022-07-23 23:26:11 +0200

    moved links to body

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,23 +1,14 @@
 <script lang="ts">
 	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
-	import Button, { Label } from '@smui/button';
 </script>
 
 <TopAppBar variant="static" style="padding: 0">
-	<Row>
+	<Row style="padding-left: 2em; padding-right: 2em">
 		<Section>
 			<Title>Matt "double v"</Title>
 		</Section>
-		<Section align="end" toolbar>
-			<Button href="https://github.com/0xvv">
-				<Label>Github</Label>
-			</Button>
-			<Button href="https://twitter.com/0xvv_sec">
-				<Label>Twitter</Label>
-			</Button>
-			<Button href="mailto:public_double_v@protonmail.com">
-				<Label>Email</Label>
-			</Button>
+		<Section align="end">
+			<h4>Crypto and cyber security</h4>
 		</Section>
 	</Row>
 </TopAppBar>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,40 @@
-<div style="text-align: center">
-	<h1>oh shit waddup</h1>
-	<p>
-		<a href="https://www.youtube.com/watch?v=FoUWHfh733Y"
+<script lang="ts">
+	import Button, {Label} from "@smui/button";
+</script>
+
+<div class="center">
+	<div class="container">
+		<h1>Useful links</h1>
+		<Button variant="unelevated" href="https://github.com/0xvv">
+			<Label>Github</Label>
+		</Button>
+		<Button variant="unelevated" href="https://twitter.com/0xvv_sec">
+			<Label>Twitter</Label>
+		</Button>
+		<Button variant="unelevated" href="mailto:public_double_v@protonmail.com">
+			<Label>Email</Label>
+		</Button>
+		<p>
+			<a href="https://www.youtube.com/watch?v=FoUWHfh733Y"
 			>Drink all the booze, hack all the things</a
-		>
-	</p>
+			>
+		</p>
+	</div>
+
 </div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		width: 500px;
+		justify-content: space-evenly;
+		height: 350px;
+	}
+
+	.center{
+		display: flex;
+		justify-content: center;
+		flex-direction: row;
+	}
+</style>
