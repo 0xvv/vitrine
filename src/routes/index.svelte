d0650e6ed96ab31d403e4c5218a3d3f626ad5bdf
0xvv <public_double_v@protonmail.com>
2022-08-22 23:42:24 +0200

    new layout, wow much modern

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,38 +4,53 @@
 
 <div class="col">
 	<div class="center">
-		<div class="">
+		<div style="width: 1000px">
 			<h2>I'm looking for work</h2>
-			<p>I have a vast cyber security culture both in web3 and web2 built over years and more recently fell down the MEV rabbit hole. </p>
-			<p>I think i would be a good fit in blockchain security, MEV or an adjacent field, thanks to my adversarial mindset and natural interest about these topics</p>
+			<p>
+				I have a vast cyber security culture both in web3 and web2 built over years and more
+				recently fell down the MEV rabbit hole.
+			</p>
+			<p>
+				I think i would be a good fit in blockchain security, MEV or an adjacent field, thanks to my
+				adversarial mindset and natural interest about these topics
+			</p>
 			<p>My goal is to help build better, more secure software and enhance user experience</p>
-			<p>Don't hesitate to contact me to chat or if you want my resume, see Twitter or Email below.</p>
+			<p>
+				Don't hesitate to contact me to chat or if you want my resume, see Twitter or Email below.
+			</p>
 		</div>
 	</div>
-	<div class="center">
-		<div class="col container">
-			<a href="/track" class="track"><h1>Useful links</h1></a>
-			<Button variant="unelevated" href="https://github.com/0xvv">
-				<Label>Github</Label>
-			</Button>
-			<Button variant="unelevated" href="https://twitter.com/0xvv_sec">
-				<Label>Twitter</Label>
-			</Button>
-			<Button variant="unelevated" href="mailto:public_double_v@protonmail.com">
-				<Label>Email</Label>
-			</Button>
+	<div class="row">
+		<div class="center">
+			<div class="container">
+				<h2>Notable work</h2>
+				<p>Contributions to <a href="https://github.com/foundry-rs">foundry-rs</a></p>
+				<p>
+					Basic Rust client for <a href="https://docs.snowsight.chainsight.dev/"
+						>snowsight mempool streaming service</a
+					>
+				</p>
+				<p>
+					Scored 32nd in <a href="https://ctf.paradigm.xyz">Paradigm CTF 2022</a> with the
+					<a href="https://skidsdao.xyz">SkidsDAO team</a>
+				</p>
+			</div>
 		</div>
-	</div>
-	<div class="center">
-		<div class="container">
-			<h2>Notable work</h2>
-			<p>Contributions to <a href="https://github.com/foundry-rs">foundry-rs</a></p>
-			<p>
-				Basic Rust client for <a href="https://docs.snowsight.chainsight.dev/">snowsight mempool streaming service</a>
-			</p>
-			<p>
-				Scored 32nd in <a href="https://ctf.paradigm.xyz">Paradigm CTF 2022</a> with the <a href="https://skidsdao.xyz">SkidsDAO team</a>
-			</p>
+		<div class="center">
+			<div class="col container start">
+				<a href="/track" class="track"><h2>Useful links</h2></a>
+				<div class="row gap">
+					<Button variant="unelevated" href="https://github.com/0xvv">
+						<Label>Github</Label>
+					</Button>
+					<Button variant="unelevated" href="https://twitter.com/0xvv_sec">
+						<Label>Twitter</Label>
+					</Button>
+					<Button variant="unelevated" href="mailto:public_double_v@protonmail.com">
+						<Label>Email</Label>
+					</Button>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
@@ -47,9 +62,13 @@
 		justify-content: space-evenly;
 	}
 
+	.start {
+		justify-content: flex-start;
+	}
+
 	.container {
 		width: 500px;
-		height: 300px;
+		height: 200px;
 	}
 
 	.center {
@@ -62,5 +81,15 @@
 		color: inherit;
 		text-decoration: inherit;
 		cursor: inherit;
+	}
+
+	.row {
+		display: flex;
+		justify-content: center;
+	}
+
+	.gap {
+		padding-top: 2em;
+		gap: 1em;
 	}
 </style>
